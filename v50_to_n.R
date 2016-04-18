a44b0189c03752e145980efd2921448126c59935
Andrew Owens <andy@bitfountain.co>
2016-04-18 19:14:20 -0400

    Update v50_to_n.R

--- v50_to_n.R
+++ v50_to_n.R
@@ -1,7 +1,7 @@
 library(corrplot)
 library(ggplot2)
 
-#### DRAFT VERSION FOR TESTING PURPOSES
+#### DRAFT VERSION FOR TESTING PURPOSES ####
 
 st_path = '/Users/aowens/maine-capstone/data' #must be changed
 d_file_2014 = "LoanStats3c.csv"
@@ -17,31 +17,27 @@ d14$status[d14$loan_status == "Fully Paid"] = "Good"
 d14$status[d14$loan_status == "In Grace Period"] = "Good"
 d14$status[d14$loan_status == "Default"] = "Bad"
 
-d14_50 = d15[50:112]
+d14_50 = d14[50:112]
 
 #FOR EACH VARIABLE IN THE DF:
-
-#generate a summary of the varibles
-summary(d14_50$var1)
-
-
-#if class(d14_50$var1) != "integer":
-
-#ELIF class(d14_50$var1) == "integer":
-#For each integer variable:
-#generate a histogram
-plot=qplot(var1, data=data, geom="histogram") 
-ggsave(plot,file="graph1.pdf")
-
-#generate a box and whisker plot for the status & loan status variables
-#status
-boxplot(var1~status,data=d14_50, main="Car Milage Data", xlab="Loan Status", ylab=names())
-
-#generate a list of integer objects for the correlation plot
 num_list = c()
+for (name in names(d14[50:112])){
+  #generate a summary of the varibles
+  summary(d14_50[name])
+  #loop through non-character variables:
+  if (sapply(d14_50[name],class) != "character" and sapply(d14_50[name],class) != "logical"){
+    #generate a list of integer objects for the correlation plot
+    num_list <- c(num_list,name)
+    #generate a histogram for each
+    plot=qplot(name, data=data, geom="histogram") 
+    ggsave(plot,file=paste(name[1]," histogram.pdf"))
+    #generate a box and whisker plot for the status & loan status variables 
+    boxplot(name~status,data=d14_50, main="Car Milage Data", xlab="Loan Status", ylab=names())
+  }
+}
 
 #get the correlation for each of the variables
-M <- cor(d14_50[num_list],use="complete")x
+M <- cor(d14_50[num_list],use="complete")
 corrplot(M, method = "circle")
 
 #plot a scatterplot of all numberic variables
