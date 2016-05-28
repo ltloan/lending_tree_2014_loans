13f4b938e5730db8d281585322f8cafc656ab766
Andrew Owens <andy@bitfountain.co>
2016-05-28 16:18:04 -0400

    Segmentation summary statistics inserted

--- loan_segmentation_cluster.R
+++ loan_segmentation_cluster.R
@@ -5,16 +5,16 @@
 library(doBy)
 library(fpc)
 library(rgl)
+library(plyr)
 
-file_data <- read.csv("/Users/aowens/Downloads/cleandata_roi.csv", stringsAsFactors=FALSE)
+#### KMEANS APPROACH: INSUFFICIENT
 
 #Performs clusters for 2:15 clusters and plots the results to use the "Elbow Method" against
 display_cluster_sizes <- function(mydata, gTitle='K-Means Elbow Plot') { 
   set.seed(1234)
   # Determine number of clusters
   wss <- (nrow(mydata)-1)*sum(apply(mydata,2,var))
-  for (i in 2:15) wss[i] <- sum(kmeans(mydata,
-                                       centers=i)$withinss)
+  for (i in 2:15) wss[i] <- sum(kmeans(mydata,centers=i)$withinss)
   plot(1:15, wss, type="b", xlab="Number of Clusters",
        ylab="Within groups sum of squares",
        main=gTitle) 
@@ -44,16 +44,52 @@ show_cluster_summary <- function(df, varInForm) {
   summaryBy(data=df, fmla, FUN=c(mean,sd,median))
 }
 
-#Remove the first column from the clustered data
-mydata <- subset(file_data, select=-Page)
+file_data <- read.csv("/Users/aowens/Downloads/cleandata_roi.csv", stringsAsFactors=FALSE)
 
-#Normalize the dataset
-scaled_data <- normalize(mydata)
-model1 <- generate_best_cluster_model(scaled_data)
+#Selection of core KPIs in numeric format
+f <- file_data[,c('pctROI','funded_amnt','annual_inc')]
+f <- normalize(f)
+
+#plot the elbow curve
+display_cluster_sizes(f)
+
+#generate the best model
+model1 <- generate_best_cluster_model(f,totalCls=5)
+
+#get the model details
+model1
+summary(model1)
+
+#write the clusters back to the initial dataframe for EDA purposes
+file_data$segment <- model1$cluster
 
 #Perform the PCA
-scaled_data.pca1 <- princomp(scaled_data, cor=T, scores=T)
+mydata.pca1 <- princomp(f, cor=T, scores=T)
 
 #Generate the 3d Plot of the clusters by the first three factors - On Mac: X11 is Required
-plot3d(scaled_data.pca1$scores[,1:3], col=as.factor(model1$cluster), 
-       main='kmeans clusters for ___')
+plot3d(mydata.pca1$scores[,1:3], col=as.factor(model1$cluster), main='kmeans clusters for k=5 clusters')
+
+plot(mydata.pca1, "Explained Variance by Principle Component Used in Visualization")
+
+#### SUMMARY STATISTICS & VISUALIZATIONS BY SEGMENT: 
+
+#Frequency Distritbution of the Clusters:
+count(file_data, 'segment')
+plot(count(file_data, 'segment'))
+
+###PULLS DATA FOR THE CORE KPIs
+#Lender Return Rate on Loan
+#Borrower Default Rate on Loan
+#Average Return Amount
+#Average Loan Term
+
+#% ROI by Segment
+boxplot(pctROI~segment,data=file_data, main="% ROI Distribution by Segment", xlab="Segment")
+#Max ROI by Segment
+boxplot(maxROI~segment,data=file_data, main="Max ROI Distribution by Segment", xlab="Segment")
+#Return amount
+file_data$amt_rtrn <- file_data$total_pymnt-file_data$total_rec_prncp
+boxplot(amt_rtrn~segment,data=file_data, main="Total Return Amount Distribution by Segment", xlab="Segment",outline=FALSE)
+
+#loan term averages
+barx <- barplot(ddply(file_data,~segment,summarise,mean=mean(term),sd=sd(term))[,c('mean')],xlab='Borrower Segment',main='Mean Loan Term by Borrower Segment',names=c("1","2","3","4","5"))
